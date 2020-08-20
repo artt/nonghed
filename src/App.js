446b9ce70fb3658d707e1d83420e59774688a039
Art Tosborvorn <art.tosborvorn@gmail.com>
2020-08-20 16:04:07 +0700

    x

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {ReactComponent as Logo} from './images/profile.svg';
 import {ReactComponent as LineSVG} from './images/line.svg';
 import {ReactComponent as FacebookSVG} from './images/facebook.svg';
 import {ReactComponent as DialogueSVG} from './images/dialogue.svg';
-import {ReactComponent as RabbitSVG} from './images/rabbit.svg';
 import {ReactComponent as GraphSVG} from './images/graph.svg';
+import {ReactComponent as PiggySVG} from './images/piggy.svg';
 import useScript from './hooks/useScript';
 
 export default function App() {
@@ -16,50 +16,86 @@ export default function App() {
 
 	return (
 		<div id="main">
-			<Logo id="logo"/>
-			<div id="description" className="section">
-				<h1>NongHedss</h1>
-				<p>
-					NongHed is your personal assistant who could provide you with
-						up-to-date economic and financial data for Thailand.
-				</p>
-				<p>
-					Simply chat using the icon in the corner below,
-						or add NongHed as a friend on LINE or Facebook!
-				</p>
+
+			<div id="header" className="section">
+				<Logo id="logo"/>
+				<div id="description">
+					<h1>NongHed</h1>
+					<p>
+						NongHed is your personal assistant who could provide you with
+							up-to-date economic and financial data for Thailand.
+						Simply chat using the icon in the corner below,
+							or add NongHed as a friend on LINE or Facebook!
+					</p>
+				</div>
+				<div id="add">
+					<div className="social-button-wrapper">
+						<a href="https://lin.ee/biOThlf" target="_blank" rel="noopener noreferrer">
+							<div className="social-button color-line">
+								<LineSVG />
+								Add Friend
+							</div>
+						</a>
+					</div>
+					<div className="social-button-wrapper">
+						<a href="https://www.facebook.com/nonghed.bot" target="_blank" rel="noopener noreferrer">
+							<div className="social-button color-facebook">
+								<FacebookSVG />
+								Facebook
+							</div>
+						</a>
+					</div>
+				</div>
 			</div>
-			<div id="add" className="section">
-				<div className="social-button-wrapper">
-					<a href="https://lin.ee/biOThlf" target="_blank" rel="noopener noreferrer">
-						<div className="social-button color-line">
-							<LineSVG />
-							Add Friend
+
+			 <div id="features" className="section">
+				<h2>Features</h2>
+				<div id="features-container">	
+					<div>
+						<div>
+							<DialogueSVG />
 						</div>
-					</a>
-				</div>
-				<div className="social-button-wrapper">
-					<a href="https://www.facebook.com/nonghed.bot" target="_blank" rel="noopener noreferrer">
-						<div className="social-button color-facebook">
-							<FacebookSVG />
-							Facebook
+						<div>
+							<h3>Natural</h3>
+							Powered by <a href="https://cloud.google.com/dialogflow/docs/">Dialogflow</a>,
+								you can talk naturally with NongHed.
+							There's no predefined pattern you need to memorize.
 						</div>
-					</a>
+					</div>
+					<div>
+						<div>
+							<GraphSVG />
+						</div>
+						<div>
+							<h3>Accurate</h3>
+							With live data from the Bank of Thailand, ThaiBMA, and more,
+								you can be sure that the data you get is up-to-date.
+						</div>
+					</div>
+					<div>
+						<div>
+							<PiggySVG />
+						</div>
+						<div>
+							<h3>Free</h3>
+							You can ask for NongHed's help at no cost.
+							We really mean it when we say, "Feel <em>free</em> to chat."
+						</div>
+					</div>
 				</div>
 			</div>
-			<div id="features" className="section">
-				<div>
-					<DialogueSVG />
-					Natural
-				</div>
-				<div>
-					<RabbitSVG />
-					Fast
-				</div>
-				<div>
-					<GraphSVG />
-					Accurate
-				</div>
+
+			<div id="data" className="section">
+				<h2>Available data</h2>
 			</div>
+			
+			<div id="contact" className="section">
+				<h2>Contact</h2>
+			</div>
+
+			<div id="bottom" className="section">
+			</div>
+			
 			<df-messenger
 			  intent="WELCOME"
 			  chat-title="Nong Hed"
@@ -71,7 +107,6 @@ export default function App() {
 	);
 }
 
-// features (fast+natural, accurate, private)
 // types of data available
 // contact
 // 
