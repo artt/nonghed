89f9be8f88efa154eab996a995b922f1f8180f39
Art Tosborvorn <art.tosborvorn@gmail.com>
2020-09-07 15:15:29 +0700

    Updates

---
+++ precache-manifest.1275499a073995806f83fa933399e6b3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f3dd834ec4b0751c2fdcef7e03d536",
+    "url": "/nonghed/index.html"
+  },
+  {
+    "revision": "b7c2f4b89bd93dbc1049",
+    "url": "/nonghed/static/css/main.bf00e7d7.chunk.css"
+  },
+  {
+    "revision": "b4040af2bf3a634815c2",
+    "url": "/nonghed/static/js/2.5c13d8c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nonghed/static/js/2.5c13d8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c2f4b89bd93dbc1049",
+    "url": "/nonghed/static/js/main.cc4a02d5.chunk.js"
+  },
+  {
+    "revision": "bde9c7111e5ea06974b8",
+    "url": "/nonghed/static/js/runtime-main.b558640d.js"
+  },
+  {
+    "revision": "efa1ad7f87d873ecf3e74af056d07e4f",
+    "url": "/nonghed/static/media/dialogue.efa1ad7f.svg"
+  },
+  {
+    "revision": "a0233c4f3ca298b727adb0443b6cd84c",
+    "url": "/nonghed/static/media/facebook.a0233c4f.svg"
+  },
+  {
+    "revision": "30d58c1ba53a9d9e7d0a20ae7116c80f",
+    "url": "/nonghed/static/media/graph.30d58c1b.svg"
+  },
+  {
+    "revision": "70b4db360ae23c998f041bedd6d23966",
+    "url": "/nonghed/static/media/kofi.70b4db36.svg"
+  },
+  {
+    "revision": "367d48649847bb1f2b774d6046e6d597",
+    "url": "/nonghed/static/media/line.367d4864.svg"
+  },
+  {
+    "revision": "6c4a212f94e4eac8405834ba936cdbd7",
+    "url": "/nonghed/static/media/piggy.6c4a212f.svg"
+  },
+  {
+    "revision": "dc29ba755798122a842a04a77981f352",
+    "url": "/nonghed/static/media/profile.dc29ba75.svg"
+  },
+  {
+    "revision": "49451d01a19312e03996e6183b68e621",
+    "url": "/nonghed/static/media/qr-line.49451d01.svg"
+  },
+  {
+    "revision": "10b73ffc9ef8ed34dd0bfd9b37b33287",
+    "url": "/nonghed/static/media/qr-promptpay.10b73ffc.svg"
+  },
+  {
+    "revision": "6cb6035f386628d96de7871cd9cef484",
+    "url": "/nonghed/static/media/qr.6cb6035f.svg"
+  }
+]);
